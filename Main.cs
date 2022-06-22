97cad915c01b7b66a4c90118a364bf57384ab6ec
mayonnaisical <85130913+mayonnaisical@users.noreply.github.com>
2022-06-22 03:10:58 -0500

    Add files via upload

--- Main.cs
+++ Main.cs
@@ -123,6 +123,14 @@ namespace Minisavestates {
 						SetValue(v[1..]);
 						break;
 
+					case "give":
+						SetTrue(v[1..]);
+						break;
+
+					case "take":
+						SetFalse(v[1..]);
+						break;
+
 					case "check":
 						CheckValue(v[1..]);
 						break;
@@ -136,6 +144,285 @@ namespace Minisavestates {
 			}
 			return false;
 
+		}
+
+		private static void SetFalse(string[] strings) {
+			var v = string.Join('\u0000', strings);
+			var value = false;
+			switch (v) {
+				case "claw":
+				case "mantisclaw":
+				case "walljump":
+				case "wallcling":
+					user.SetValue("canWallJump", value);
+					break;
+				case "wings":
+				case "doublejump":
+				case "monarchwings":
+					user.SetValue("hasDoubleJump", value);
+					break;
+				case "ismas":
+				case "ismastear":
+				case "isma's":
+				case "isma'stear":
+				case "acidswim":
+				case "acidarmour":
+					user.SetValue("hasAcidArmour", value);
+					break;
+				case "dgate":
+				case "dreamgate":
+					user.SetValue("hasDreamGate", value);
+					break;
+				case "dnail":
+				case "dreamnail":
+					user.SetValue("hasDreamNail", value);
+					user.SetValue("hasDreamGate", value);
+					break;
+				case "dash":
+				case "mothwingcloak":
+				case "cloak":
+					user.SetValue("canDash", value);
+					user.SetValue("hasDash", value);
+					break;
+				case "shadecloak":
+				case "shadowdash":
+					user.SetValue("hasShadowDash", value);
+					break;
+				case "lantern":
+				case "lumaflylantern":
+				case "lamp":
+					user.SetValue("hasLantern", value);
+					break;
+				case "cdash":
+				case "crystaldash":
+				case "crystalheart":
+				case "cheart":
+					user.SetValue("canSuperDash", value);
+					user.SetValue("hasSuperDash", value);
+					break;
+				case "greatslash":
+				case "gslash":
+					user.SetValue("hasUpwardSlash", value);
+					user.SetValue("hasAllNailArts", value);
+					break;
+				case "cycloneslash":
+				case "cyclone":
+					user.SetValue("hasCycloneSlash", value);
+					user.SetValue("hasAllNailArts", value);
+					break;
+				case "dashslash":
+				case "dslash":
+				case "dascheslasche":
+				case "thatonenailart":
+					user.SetValue("hasDashSlash", value);
+					user.SetValue("hasAllNailArts", value);
+					break;
+				case "trampass":
+				case "tram":
+				case "pass":
+					user.SetValue("hasTramPass", value);
+					break;
+				case "shopkey":
+				case "shopkeeper'skey":
+				case "shopkeeperskey":
+				case "slykey":
+					user.SetValue("hasSlyKey", value);
+					break;
+				case "citycrest":
+				case "crest":
+					user.SetValue("hasCityKey", value);
+					break;
+				case "elegantkey":
+				case "ekey":
+					user.SetValue("hasWhiteKey", value);
+					break;
+				case "lovekey":
+					user.SetValue("hasLoveKey", value);
+					break;
+				case "brand":
+				case "king'sbrand":
+				case "kingsbrand":
+					user.SetValue("hasKingsBrand", value);
+					break;
+
+				// spells, i know these aren't bools but meh
+				case "vs":
+				case "vengefulspirit":
+				case "fireball":
+					user.SetValue("fireballLevel", 0);
+					break;
+				case "shadesoul":
+					user.SetValue("fireballLevel", 1);
+					break;
+
+				case "dive":
+				case "desolatedive":
+				case "ddive":
+					user.SetValue("quakeLevel", 0);
+					break;
+				case "descendingdark":
+				case "ddark":
+					user.SetValue("quakeLevel", 1);
+					break;
+
+				case "wraiths":
+				case "howlingwraiths":
+					user.SetValue("screamLevel", 0);
+					break;
+				case "shriek":
+				case "abyssshriek":
+					user.SetValue("screamLevel", 1);
+					break;
+
+				default:
+					if (user.dat.ContainsKey(v)) {
+						user.dat[v] = false;
+					}
+					else {
+						Console.WriteLine($"Could not parse {v}");
+					}
+					break;
+			}
+
+		}
+
+		private static void SetTrue(string[] strings) {
+			var v = string.Join('\u0000', strings);
+			var value = true;
+			switch (v) {
+				case "claw":
+				case "mantisclaw":
+				case "walljump":
+				case "wallcling":
+					user.SetValue("canWallJump", value);
+					break;
+				case "wings":
+				case "doublejump":
+				case "monarchwings":
+					user.SetValue("hasDoubleJump", value);
+					break;
+				case "ismas":
+				case "ismastear":
+				case "isma's":
+				case "isma'stear":
+				case "acidswim":
+				case "acidarmour":
+					user.SetValue("hasAcidArmour", value);
+					break;
+				case "dgate":
+				case "dreamgate":
+					user.SetValue("hasDreamGate", value);
+					user.SetValue("hasDreamNail", value);
+					break;
+				case "dnail":
+				case "dreamnail":
+					user.SetValue("hasDreamNail", value);
+					break;
+				case "dash":
+				case "mothwingcloak":
+				case "cloak":
+					user.SetValue("canDash", value);
+					user.SetValue("hasDash", value);
+					break;
+				case "shadecloak":
+				case "shadowdash":
+					user.SetValue("hasShadowDash", value);
+					break;
+				case "lantern":
+				case "lumaflylantern":
+				case "lamp":
+					user.SetValue("hasLantern", value);
+					break;
+				case "cdash":
+				case "crystaldash":
+				case "crystalheart":
+				case "cheart":
+					user.SetValue("canSuperDash", value);
+					user.SetValue("hasSuperDash", value);
+					break;
+				case "greatslash":
+				case "gslash":
+					user.SetValue("hasUpwardSlash", value);
+					user.SetValue("hasNailArt", value);
+					break;
+				case "cycloneslash":
+				case "cyclone":
+					user.SetValue("hasCycloneSlash", value);
+					user.SetValue("hasNailArt", value);
+					break;
+				case "dashslash":
+				case "dslash":
+				case "dascheslasche":
+				case "thatonenailart":
+					user.SetValue("hasDashSlash", value);
+					user.SetValue("hasNailArt", value);
+					break;
+				case "trampass":
+				case "tram":
+				case "pass":
+					user.SetValue("hasTramPass", value);
+					break;
+				case "shopkey":
+				case "shopkeeper'skey":
+				case "shopkeeperskey":
+				case "slykey":
+					user.SetValue("hasSlyKey", value);
+					break;
+				case "citycrest":
+				case "crest":
+					user.SetValue("hasCityKey", value);
+					break;
+				case "elegantkey":
+				case "ekey":
+					user.SetValue("hasWhiteKey", value);
+					break;
+				case "lovekey":
+					user.SetValue("hasLoveKey", value);
+					break;
+				case "brand":
+				case "king'sbrand":
+				case "kingsbrand":
+					user.SetValue("hasKingsBrand", value);
+					break;
+
+				// spells, i know these aren't bools but meh
+				case "vs":
+				case "vengefulspirit":
+				case "fireball":
+					user.SetValue("fireballLevel", 1);
+					break;
+				case "shadesoul":
+					user.SetValue("fireballLevel", 2);
+					break;
+
+				case "dive":
+				case "desolatedive":
+				case "ddive":
+					user.SetValue("quakeLevel", 1);
+					break;
+				case "descendingdark":
+				case "ddark":
+					user.SetValue("quakeLevel", 2);
+					break;
+
+				case "wraiths":
+				case "howlingwraiths":
+					user.SetValue("screamLevel", 1);
+					break;
+				case "shriek":
+				case "abyssshriek":
+					user.SetValue("screamLevel", 2);
+					break;
+
+				default:
+					if (user.dat.ContainsKey(v)) {
+						user.dat[v] = false;
+					}
+					else {
+						Console.WriteLine($"Could not parse {v}");
+					}
+					break;
+			}
 		}
 
 		private static void SetValue(string[] v) {
@@ -160,6 +447,22 @@ namespace Minisavestates {
 				case "geo":
 					user.SetValue("geo", int.Parse(v[1]));
 					break;
+				case "essence":
+					user.SetValue("dreamOrbs", int.Parse(v[1]));
+					break;
+				case "soul":
+					var soul = int.Parse(v[1]);
+					var res = soul > 100 ? soul % 99 : 0;
+					user.SetValue("MPCharge", soul);
+					user.SetValue("MPReserve", soul);
+					break;
+				case "vessels":
+					var x = int.Parse(v[1]);
+					x = x >= 3 ? 3 : x;
+					x *= 33;
+					user.SetValue("MPReserveMax", x);
+					break;
+
 				default:
 					Console.WriteLine($"Could not parse {v[0]}");
 					return;
